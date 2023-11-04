6dd44cab82089ceb2d0fccf867ee84a3a6292887
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2023-11-04 00:27:43 -0300

    Se deja la api en modo debug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-6f*nl8qj3^7d0abew$s=(o3&m&_46jul8zy8a=(&pockchi#-w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
