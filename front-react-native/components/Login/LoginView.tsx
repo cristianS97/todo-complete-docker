093488bace695d0c4ddcf719d7df155817da6b4e
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2023-12-09 12:26:51 -0300

    Se habilita login y se obtiene token en pagina de login

--- front-react-native/components/Login/LoginView.tsx
+++ front-react-native/components/Login/LoginView.tsx
@@ -3,34 +3,61 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { InputLogin } from './InputLogin';
 import { ButtonLogin } from './ButtonLogin';
+import { isConstructorDeclaration } from 'typescript';
 
 export default function LoginView({navigation}) {
     const [username, setUsername] = useState<string>('')
     const [password, setPassword] = useState<string>('')
 
+    const doLogin = async () => {
+        const ipv4 = '';
+
+        if(username.trim() !== '' && password.trim() !== '') {
+            var raw = JSON.stringify({
+                "username": username,
+                "password": password
+            });
+            const resp = await fetch(`http://${ipv4}:8000/auth/login/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: raw,
+                redirect: 'follow'
+            });
+            const body = await resp.json();
+            if(resp.ok) {
+                //navigation.navigate('todos')
+                alert(`Login exitoso, token: ${body.token}`)
+            } else {
+                alert(`Error login, ${body.detail}`)
+            }
+        }
+    }
+
     return (
         <View style={styles.body}>
             <ImageBackground source={require('../../assets/mias/coffeeBGBlur.jpg')} style={styles.body}>
-            <View style={styles.container}>
-                <Text style={styles.title}>Login</Text>
-                <InputLogin
-                    label="Usuario"
-                    value={username}
-                    onChange={setUsername}
-                    security={false}
-                />
-                <InputLogin
-                    label="Contraseña"
-                    value={password}
-                    onChange={setPassword}
-                    security={true}
-                />
-                <ButtonLogin
-                    text="Ingresar"
-                    onPress={() => navigation.navigate('todos')}
-                />
-                <StatusBar style="light" />
-            </View>
+                <View style={styles.container}>
+                    <Text style={styles.title}>Login</Text>
+                    <InputLogin
+                        label="Usuario"
+                        value={username}
+                        onChange={setUsername}
+                        security={false}
+                    />
+                    <InputLogin
+                        label="Contraseña"
+                        value={password}
+                        onChange={setPassword}
+                        security={true}
+                    />
+                    <ButtonLogin
+                        text="Ingresar"
+                        onPress={doLogin}
+                    />
+                    <StatusBar style="light" />
+                </View>
             </ImageBackground>
         </View>
     );
